9432096d118095ef19af5eeb45510fcaad31ac5a
MINEDUCYT <MINEDUCYT@DESKTOP-GLJR0B3>
2025-03-27 16:40:34 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+	namespace videojuego
+	{
+		internal class Program
+		{
+
+			public enum Raza { Humano, Elfo, Enano, Orco }
+			public enum Clase { Guerrero, Mago, Arquero, Picaro }
+
+			public class Personaje
+			{
+				public string Nombre { get; set; }
+				public Raza Raza { get; set; }
+				public Clase Clase { get; set; }
+				public int Fuerza { get; set; }
+				public int Inteligencia { get; set; }
+				public int Destreza { get; set; }
+
+				public Personaje(string nombre, Raza raza, Clase clase, int fuerza, int inteligencia, int destreza)
+				{
+					Nombre = nombre;
+					Raza = raza;
+					Clase = clase;
+					Fuerza = fuerza;
+					Inteligencia = inteligencia;
+					Destreza = destreza;
+				}
+
+			}
+
+			public class Programs
+			{
+				public static void Main(string[] args)
+				{
+					Console.WriteLine("Creador de personaje ");
+
+					Console.Write("Nombre del personaje: ");
+					string nombre = Console.ReadLine();
+
+					Console.WriteLine("Elige la raza:");
+					foreach (Raza raza in Enum.GetValues(typeof(Raza)))
+					{
+						Console.WriteLine($"{(int)raza}. {raza}");
+					}
+					Raza razaElegida = (Raza)int.Parse(Console.ReadLine());
+
+					Console.WriteLine("Elige la clase:");
+					foreach (Clase clase in Enum.GetValues(typeof(Clase)))
+					{
+						Console.WriteLine($"{(int)clase}. {clase}");
+					}
+					Clase claseElegida = (Clase)int.Parse(Console.ReadLine());
+
+					Console.WriteLine("Asigna puntos a las estadísticas (total 30 puntos):");
+					Console.Write("Fuerza: ");
+					int fuerza = int.Parse(Console.ReadLine());
+					Console.Write("Inteligencia: ");
+					int inteligencia = int.Parse(Console.ReadLine());
+					Console.Write("Destreza: ");
+					int destreza = int.Parse(Console.ReadLine());
+
+					Personaje nuevoPersonaje = new Personaje(nombre, razaElegida, claseElegida, fuerza, inteligencia, destreza);
+
+					Console.WriteLine("\nPersonaje creado:");
+					Console.WriteLine($"Nombre: {nuevoPersonaje.Nombre}");
+					Console.WriteLine($"Raza: {nuevoPersonaje.Raza}");
+					Console.WriteLine($"Clase: {nuevoPersonaje.Clase}");
+					Console.WriteLine($"Fuerza: {nuevoPersonaje.Fuerza}");
+					Console.WriteLine($"Inteligencia: {nuevoPersonaje.Inteligencia}");
+					Console.WriteLine($"Destreza: {nuevoPersonaje.Destreza}");
+				}
+			}
+		}
+
+	}
+}
---
+++ ConsoleApp28/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleApp28")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp28")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("42fe6a61-0aab-47d1-8d7d-7fbd80ea8f7c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
